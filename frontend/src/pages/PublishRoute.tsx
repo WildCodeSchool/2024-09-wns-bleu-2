6774c8fab03565a1b401a07678a6f81a20e5fdf7
Nelia <nelia.courtais@live.fr>
2025-03-11 17:13:31 +0100

    work in progress

---
+++ frontend/src/pages/PublishRoute.tsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+// import SearchBar from "../components/SearchBar";
+
+
+const PublishRoute = () => {
+  // const [search, setSearch] = useState("");
+
+  return (
+    <div>
+      <h2>Publier une Route</h2>
+      {/* <SearchBar value={search} onChange={(e) => setSearch(e.target.value)} /> */}
+      <p>Résultats pour : search</p>
+    </div>
+  );
+};
+
+export default PublishRoute;
