c49d0a8c8523971540fb694ae0b231ed558c9281
Nelia <nelia.courtais@live.fr>
2025-04-16 10:08:38 +0200

    finish back for Book a trip page + change price selector

--- frontend/src/pages/BookATripPage.tsx
+++ frontend/src/pages/BookATripPage.tsx
@@ -1,18 +1,63 @@
-import { useParams } from "react-router-dom";
-import { Carpool, useGetCarpoolByIdQuery } from "../generated/graphql-types";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+  Carpool,
+  useCreateBookingMutation,
+  useGetCarpoolByIdQuery,
+  useGetUserInfoQuery,
+} from "../generated/graphql-types";
 
 import "../styles/trip-cards.scss";
 import TripSingleCard from "../components/TripSingleCard";
 import { formatLongDate } from "../utils/dateUtils";
 import DriverInfo from "../components/bookATripPageComponents/DriverInfo";
 import BookingSummaryCard from "../components/bookATripPageComponents/BookingSummaryCard";
+import { toast } from "react-toastify";
 
 const BookATripPage = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
 
   const { data, loading, error } = useGetCarpoolByIdQuery({
     variables: { getCarpoolByIdId: Number(id) },
   });
+
+  const { data: userData } = useGetUserInfoQuery();
+  const userId = userData?.getUserInfo.id;
+
+  const [creatBooking] = useCreateBookingMutation();
+
+  const handleBooking = async () => {
+    if (!userId) {
+      toast.error("Vous devez être connecté pour réserver un trajet.");
+      return;
+    }
+
+    const alreadyBooked = data?.getCarpoolById.bookings.some(
+      (booking) => booking.passenger.id === userId
+    );
+
+    if (alreadyBooked) {
+      toast.error("Vous avez déjà réservé ce trajet.");
+      return;
+    }
+
+    try {
+      await creatBooking({
+        variables: {
+          data: {
+            carpool_id: Number(id),
+            passenger_id: userId,
+            numPassenger: 1,
+            reservedAt: new Date().toISOString(),
+          },
+        },
+      });
+      toast.success("Réservation réussie !");
+      navigate(`/myreservations/${userId}`);
+    } catch (error) {
+      toast.error("Erreur lors de la réservation.");
+    }
+  };
 
   if (loading) return <p>Chargement...</p>;
   if (error || !data?.getCarpoolById)
@@ -27,7 +72,7 @@ const BookATripPage = () => {
         <BookingSummaryCard
           carpool={data.getCarpoolById as Carpool}
           numPassengers={1}
-          onBook={() => console.log("Réservation confirmée !")}
+          onBook={handleBooking}
         />
       </div>
     </div>
