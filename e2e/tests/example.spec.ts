ad2c9bda7fda88cab5375f6b7dd45c1d980eb212
MMBIKAI <mohanad_bikai@hotmail.com>
2025-06-10 16:09:48 +0200

    test workflow for only example.spec.ts

--- e2e/tests/example.spec.ts
+++ e2e/tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("Go to home page", async ({ page }) => {
-  await page.goto("http://api_gateway:80/");
+  await page.goto("http://localhost:8000/");
   await page.waitForLoadState("networkidle");
 
   // Expect homepage image is visible
@@ -9,6 +9,6 @@ test("Go to home page", async ({ page }) => {
   //await expect(image).toBeVisible(    { timeout: 15000 });
 });
 test("Verify Nginx Routing", async ({ page }) => {
-  const response = await page.goto("http://api_gateway:80/");
+  const response = await page.goto("http://localhost:8000/");
   expect(response?.status()).toBe(200);  // Should now pass
 });
