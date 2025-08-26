c493de12e4962691216ad4e9ac930d0b259c7bff
Océane Lahoual <138216585+Midunighto@users.noreply.github.com>
2025-08-26 15:42:52 +0200

    fix damaged files

---
+++ frontend/src/contexts/ModalContext.tsx
@@ -0,0 +1,38 @@
+import { createContext, useContext, useState, ReactNode } from "react";
+
+type ModalContextType = {
+  isLoginModalOpen: boolean;
+  setIsLoginModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  redirectAfterLogin: string | null;
+  setRedirectAfterLogin: React.Dispatch<React.SetStateAction<string | null>>;
+};
+
+const ModalContext = createContext<ModalContextType | undefined>(undefined);
+
+export const ModalProvider = ({ children }: { children: ReactNode }) => {
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+  const [redirectAfterLogin, setRedirectAfterLogin] = useState<string | null>(
+    null
+  );
+
+  return (
+    <ModalContext.Provider
+      value={{
+        isLoginModalOpen,
+        setIsLoginModalOpen,
+        redirectAfterLogin,
+        setRedirectAfterLogin,
+      }}
+    >
+      {children}
+    </ModalContext.Provider>
+  );
+};
+
+export const useModal = () => {
+  const context = useContext(ModalContext);
+  if (!context) {
+    throw new Error("useModal must be used within a ModalProvider");
+  }
+  return context;
+};
